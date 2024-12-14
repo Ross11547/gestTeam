527ece9b152583e806c98089252cfe7a6f7364e9
Ross11547 <ross.trujillo.16.com>
2024-12-14 17:02:55 -0400

    gestteam

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import Login from "./pages/public/login"
+
+
+function App() {
+
+  return (
+    <>
+      <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/style/colors.jsx
@@ -0,0 +1,29 @@
+export const ColorsEstu = {
+    // Amarillos
+    primary: "#FFCC00",   
+    primary100: "#FFDE59", 
+    primary200: "#FFE680", 
+    primary300: "#FFD700",
+    // Amarillos Oscuros
+    primary400: "#E6B800",
+    primary500: "#CC9A00", 
+    primary600: "#B38600", 
+    primary700: "#996F00", 
+};
+
+export const ColorsLogin = {
+    // Naranjas
+    secondary100: "#F89C5E", 
+    secondary200: "#F06724", 
+    secondary300: "#E67E00", 
+    secondary400: "#CC7000", 
+    secondary500: "#B35C00", 
+};
+
+export const Colors = {
+    // Blancos y Grises
+    white: "#FFFFFF",      
+    greyLight: "#E6E6E6",  
+    greyDark: "#4D4D4D",   
+    black: "#000000"       
+};
