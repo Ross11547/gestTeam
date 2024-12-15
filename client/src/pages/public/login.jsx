58dd037aec78b0afb6ec92104c8d6b84b30a8e7e
Ross11547 <ross.trujillo.16.com>
2024-12-15 19:52:07 -0400

    loginTerminator

--- client/src/pages/public/login.jsx
+++ client/src/pages/public/login.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  LoginContainer,
+  BackgroundImage,
+  LoginFormContainer,
+  LoginTitle,
+  LoginButton,
+  LogoImage,
+  LogoContainer,
+  Form,
+  InputWrapper,
+  StyledLabel,
+  StyledInput,
+} from "../../style/loginStyle.jsx";
+import Logo from "../../assets/img/logoDos.png";
+import Fondo from "../../assets/img/FondoCinco.png";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [isActive, setIsActive] = useState(false);
 
-export default Login
+  const handleFocus = () => setIsActive(true);
+  const handleBlur = (e) => setIsActive(e.target.value !== "");
+  return (
+    <LoginContainer>
+      <BackgroundImage src={Fondo} alt="Imagen de fondo" />
+      <LoginFormContainer>
+        <LogoContainer>
+          <LogoImage src={{}} alt="" />
+        </LogoContainer>
+        <LoginTitle>Login</LoginTitle>
+        <Form>
+          <InputWrapper>
+            <StyledInput active={isActive} onFocus={handleFocus} onBlur={handleBlur} type="email"/>
+            <StyledLabel active={isActive}>Correo</StyledLabel>
+          </InputWrapper>
+          <InputWrapper>
+            <StyledInput active={isActive} onFocus={handleFocus} onBlur={handleBlur} type="password"/>
+            <StyledLabel active={isActive}>Contraseña</StyledLabel>
+          </InputWrapper>
+          <LoginButton>Ingresar</LoginButton>
+        </Form>
+      </LoginFormContainer>
+    </LoginContainer>
+  );
+};
+
+export default Login;
